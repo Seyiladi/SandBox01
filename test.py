88f9c4742425ea534893e164558b7982d507899e
Seyiladi <seyiladi@gmail.com>
2025-02-05 12:50:54 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def split_string_by_space(input_string):
     return input_string.split(' ')
 
-# Example usage
+# Example usage.
 
 test_string = "This is a test string"
 print(split_string_by_space(test_string))
